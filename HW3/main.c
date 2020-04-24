2e3f6c05b706e1aa011983b24f459d29b6145119
Aaron <ape2020@u.northwestern.edu>
2020-04-23 23:14:37 -0500

    code works for hw3

--- HW3/main.c
+++ HW3/main.c
@@ -1,7 +1,9 @@
 #include<xc.h>           // processor SFR definitions
 #include<sys/attribs.h>  // __ISR macro
-#include "spi.h"
-#include "dac.h"
+//#include "spi.h"
+//#include "dac.h"
+#include "i2c_master_noint.h"
+#include "mcp23017.h"
 
 // DEVCFG0
 #pragma config DEBUG = OFF // disable debugging
@@ -34,12 +36,24 @@
 #pragma config PMDL1WAY = OFF // allow multiple reconfigurations
 #pragma config IOL1WAY = OFF // allow multiple reconfigurations
 
-// Delay for 1 seconds    #TODO: Abstract this to delay for any number of seconds
+// Delay for 1 seconds   (24M counts in 1 sec) 
 void delay(void) {
     _CP0_SET_COUNT(0);
-    while (_CP0_GET_COUNT() < 240000) {     // idk why its 177k lmfao
+    while (_CP0_GET_COUNT() < 240000) {     
         // Do nothing for 10ms (100Hz)
     }
+}
+
+// Blink A4 for 100ms
+void heartbeat(void) {
+    LATAbits.LATA4 = 1;         // High
+    _CP0_SET_COUNT(0);
+    while (_CP0_GET_COUNT() < 48000000 / 2 / 10) {
+    }
+    LATAbits.LATA4 = 0;         // Low
+    _CP0_SET_COUNT(0);
+    while (_CP0_GET_COUNT() < 48000000 / 2 / 10) {
+    }    
 }
 
 int main() {
@@ -58,56 +72,25 @@ int main() {
     // disable JTAG to get pins back
     DDPCONbits.JTAGEN = 0;
 
-    // do your TRIS and LAT commands here
-    TRISBbits.TRISB4 = 1;       // Set B4 as input
+    // Set A4 as output to blink for heartbeat
     TRISAbits.TRISA4 = 0;       // Set A4 as output
     LATAbits.LATA4 = 0;         // Initially off
-    
-    // Homework 2 SPI
-    initSPI();
+
+    // Enable I2C1 (HW3)
+    i2c_master_setup();     // enable i2c
+    mcp_init();             // init mcp with B as inputs and A as outputs
     
     __builtin_enable_interrupts();
    
-    // Create 2 arrays to represent 1 period of each wave (recall 12-bit resolution => 4096)
-    unsigned short triangle[100];
-    unsigned short sine[50];
-    int i = 0;        // index for triangle wave @ 1Hz
-    int j = 0;        // index for sine wave @ 2Hz
-
-    // Populate triangle wave
-    int ii;
-    for (ii = 0; ii < 100; ii++) {
-        if (ii < 50) {     // up
-            triangle[ii] = (unsigned short)(4096/50 * ii);
-        } else {           // down
-            triangle[ii] = triangle[99-ii];     // Just go in reverse
-        }
-    }
-
-    // Populate sine wave
-    int jj;
-    for (jj = 0; jj < 50; jj++) {
-        sine[jj] = get_sine(jj);
-    }
-
-
     while (1) {
-        // Write the bytes to the DAC
-        dac_write('a', triangle[i]);
-        dac_write('b', sine[j]);
-
-        i++;
-        j++;
-        // Reset indexing for triangle wave to start of period
-        if (i == 100) {
-            i = 0;
+        // Read pin B
+        unsigned char bVals = readPinB(MCP_ADDRESS);
+        // B0 is pull-up
+        if (bVals & 0x1) {
+            clearPinA(MCP_ADDRESS, 7);  // turn off if not pressed
+        } else {
+            setPinA(MCP_ADDRESS,7);     // turn on the yellow led
         }
-        // Reset indexing for sine wave to start of period
-        if (j == 50) {
-            j = 0;
-        }
-
-        //DELAY => remember this runs at half the sysclock, so 24MHz
-        delay();        // This is how fast the DAC updates(?) => Delay for 10ms
+        heartbeat();
     }
 }
